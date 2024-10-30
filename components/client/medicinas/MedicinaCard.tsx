fb37995c25dd0acfe3df82fa7bac5c8217fb8aea
Ismael Catala <ismael.catala@smileandlearn.com>
2024-10-30 20:27:34 +0100

    feat:

--- components/client/medicinas/MedicinaCard.tsx
+++ components/client/medicinas/MedicinaCard.tsx
@@ -9,6 +9,8 @@ import {
   } from "@/components/ui/card"
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Badge } from "@/components/ui/badge"
+
 export interface Medicina {
     id: number;
     nombre: string;
@@ -21,7 +23,7 @@ export interface Medicina {
   }
 export default function MedicinaCard({medicine}: MedicinaCardProps) {
   return (
-        <Card className="">
+        <Card className="hover:shadow-lg transition-all duration-300 ease-in-out cursor-pointer">
             <CardHeader>
                 <AspectRatio ratio={16 / 9}>
                     <Image src={medicine.imagen} alt={medicine.nombre} className="rounded-md object-cover" layout="fill" objectFit="cover" />
@@ -29,6 +31,13 @@ export default function MedicinaCard({medicine}: MedicinaCardProps) {
                 <CardTitle>{medicine.nombre}</CardTitle>
                 <CardDescription>{medicine.descripcion}</CardDescription>
             </CardHeader>
+            <CardFooter>
+                <div className="flex items-center justify-between">
+                    <Badge  className="text-xs bg-purple-800 text-purple-200" >
+                        {medicine.nombre}
+                    </Badge>
+                </div>
+            </CardFooter>
            
             
         </Card>
